249c1907e428116333b6b76f5eb3a40f24d8cbe2
Hrihorii Ilin <frikadellios@gmail.com>
2024-12-04 18:14:20 +0000

    add tailwind 3 for starter

--- astro.config.ts
+++ astro.config.ts
@@ -1,20 +1,81 @@
 // @ts-check
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import AutoImport from "unplugin-auto-import/astro";
+import cloudflare from "@astrojs/cloudflare";
+import * as path from "node:path";
+import react from "@astrojs/react";
 
-import cloudflare from '@astrojs/cloudflare';
+import partytown from "@astrojs/partytown";
 
-import react from '@astrojs/react';
+import tailwind from "@astrojs/tailwind";
+import postCssOklabPolyfill from "@csstools/postcss-oklab-function";
+import autoprefixer from "autoprefixer";
+import cssDiscardComments from "postcss-discard-comments";
+import tailwindcss from "tailwindcss";
+import tailwindcssNesting from "tailwindcss/nesting";
 
-// https://astro.build/config
+
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react()],
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true
-    }
-  }),
+	site: "https://example.com",
+	prefetch: {
+		prefetchAll: true,
+		defaultStrategy: "viewport",
+	},
+	vite: {
+		css: {
+			postcss: {
+				plugins: [
+					tailwindcssNesting(),
+					tailwindcss({
+						config: path.resolve(import.meta.dirname, "tailwind.config.ts"),
+					}),
+					postCssOklabPolyfill({ preserve: true }),
+					autoprefixer(),
+					cssDiscardComments({ removeAll: true }),
+				],
+			},
+		},
+		ssr: {
+			external: ["node:buffer", "three"],
+		},
+	},
+	integrations: [
+		mdx({
+			syntaxHighlight: "shiki",
+			shikiConfig: {
+				theme: "github-dark-dimmed",
+			},
+			gfm: true,
+		}),
+		sitemap(),
+		react(),
+		AutoImport({
+			defaultExportByFilename: false,
+			include: [/\.[tj]sx?$/, /\.md$/],
+			packagePresets: ["detect-browser-es"],
+			imports: ["react", "react-router", "react-i18next"],
+			viteOptimizeDeps: true,
+			injectAtEnd: true,
+			dirs: ["./src/utils/*.ts", "./src/hooks"],
+			dts: ".astro/auto-imports.d.ts",
+		}),
+		partytown({
+			config: {
+				forward: ["dataLayer.push"],
+			},
+		}),
+		tailwind({
+			nesting: true,
+			applyBaseStyles: false,
+		}),
+	],
+	adapter: cloudflare({
+		imageService: "cloudflare",
+		platformProxy: {
+			enabled: true,
+		},
+	}),
 });
